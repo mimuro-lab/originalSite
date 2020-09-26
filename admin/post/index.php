8c8dfed522a999fc28ba8c5d2bbd0d9d7b9f2c02
mimuro-lab <mimurosyunya@gmail.com>
2020-09-26 22:38:21 +0900

    投稿用のフォームの作成

--- admin/post/index.php
+++ admin/post/index.php
@@ -1,4 +1,7 @@
-﻿<!DOCTYPE html>
+﻿<?php
+require(".//..//check.php");
+?>
+<!DOCTYPE html>
 <html>
   <head>
     <title>タイトル　編集画面</title>
@@ -6,7 +9,10 @@
   <body>
     <?php
     if(isset($_COOKIE["username"]) && isset($_COOKIE["password"])){
-        echo $_COOKIE["username"] . ":" . $_COOKIE["password"] . "<br>";
+        if(check_userInfo($_COOKIE["username"], $_COOKIE["password"])){
+          echo $_COOKIE["username"] . "さんの投稿<br>";
+          require(".//form_post.html");
+        }
     }
     else{
         echo "ユーザー情報が入力されていません。";
