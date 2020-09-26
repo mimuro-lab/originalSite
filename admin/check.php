39e5b667387ea01cc629dcf0fe7c9fbabdbcf9a6
mimuro-lab <mimurosyunya@gmail.com>
2020-09-26 19:54:49 +0900

    ユーザー情報を確認する関数

---
+++ admin/check.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+function check_userInfo(string $username, string $passward){
+    $fp = fopen(__DIR__."\\admin.csv", "r");
+    while($content = fgets($fp)){
+        if(count(explode(",",$content))!=2){
+            continue;
+        }
+        $admin_username = explode(",", $content)[0];
+        $admin_username = preg_replace('/[\x00-\x1F\x7F]/','', $admin_username);//制御文字の削除
+        $admin_password = explode(",", $content)[1];
+        $admin_password = preg_replace('/[\x00-\x1F\x7F]/','', $admin_password);//制御文字の削除
+        if($username == $admin_username && $passward == $admin_password){
+            fclose($fp);
+            return true;
+        }
+    }
+    return false;
+    fclose($fp);
+}
+
+?>
--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,16 @@
-﻿<!DOCTYPE html>
+﻿
+<?php
+require(".\\check.php");
+?>
+
+<!DOCTYPE html>
 <html>
   <head>
     <title>タイトル　管理ページ</title>
   </head>
   <body>
-    <p></p>
+  <?php
+  var_dump(check_userInfo("chinou", "chinou"));
+  ?>
   </body>
 </html>
