be91c3330ab3e99bce8a4996bdf8535e426b0299
mimuro-lab <mimurosyunya@gmail.com>
2020-09-26 22:16:57 +0900

    Cookieによりユーザ情報を保存し、編集画面を作成。

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,7 @@
 <?php
 require(".\\check.php");
 require(".\\echoHTML.php");
+
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +23,9 @@ require(".\\echoHTML.php");
     $username = $_POST["username"];
     $password = $_POST["password"];
     if(check_userInfo($username, $password)){//認証が成功したら
+      $effectiveTime = time() + 60 * 60 * 3;//有効期限は3時間
+      setcookie("username", $username, $effectiveTime);
+      setcookie("password", $password, $effectiveTime);
       echoSuccessPage();
     }else{//認証に失敗したら
       echoFailedPage();
---
+++ admin/post/index.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+    <title>タイトル　編集画面</title>
+  </head>
+  <body>
+    <?php
+    if(isset($_COOKIE["username"]) && isset($_COOKIE["password"])){
+        echo $_COOKIE["username"] . ":" . $_COOKIE["password"] . "<br>";
+    }
+    else{
+        echo "ユーザー情報が入力されていません。";
+    }
+    ?>
+  </body>
+</html>
